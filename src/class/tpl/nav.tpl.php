1bd7c648db3075d723e57c83ca325e61bb9fc227
tontof <tontof@free.fr>
2013-06-13 19:23:36 +0200

    unicode icons

--- src/class/tpl/nav.tpl.php
+++ src/class/tpl/nav.tpl.php
@@ -7,14 +7,7 @@
         menu
       </a>
 
-      <a id="nav-home" class="brand icon icon-rate" href="<?php echo MyTool::getUrl(); ?>" title="Home">
-        <span class="ico-navbar">
-          <span class="ico">
-            <span class="ico-home-square"></span>
-            <span class="ico-home-triangle"></span>
-            <span class="ico-home-line"></span>
-          </span>
-        </span>
+      <a id="nav-home" class="brand menu-ico ico-home" href="<?php echo MyTool::getUrl(); ?>" title="Home">
       </a>
 
       <?php if (isset($currentHashView)) { ?>
@@ -35,17 +28,7 @@
                 case 'menuView': ?>
           <?php if ($view === 'expanded') { ?>
           <li>
-            <a href="<?php echo $query.'view=list'; ?>" title="Switch to list view (one line per item)">
-              <span class="menu-ico ico-list">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-list-line-1"></span>
-                  <span class="ico-list-line-2"></span>
-                  <span class="ico-list-line-3"></span>
-                  <span class="ico-list-line-4"></span>
-                  <span class="ico-list-line-5"></span>
-                </span>
-              </span>
+            <a href="<?php echo $query.'view=list'; ?>" title="Switch to list view (one line per item)" class="menu-ico ico-list">
               <span class="menu-text menu-list">
                 View as list
               </span>
@@ -53,15 +36,7 @@
           </li>
           <?php } else { ?>
           <li>
-            <a href="<?php echo $query.'view=expanded'; ?>" title="Switch to expanded view">
-              <span class="menu-ico ico-expanded">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-expanded-line-1"></span>
-                  <span class="ico-expanded-line-2"></span>
-                  <span class="ico-expanded-line-3"></span>
-                </span>
-              </span>
+            <a href="<?php echo $query.'view=expanded'; ?>" title="Switch to expanded view" class="menu-ico ico-expanded">
               <span class="menu-text menu-expanded">
                 View as expanded
               </span>
@@ -72,13 +47,7 @@
           <?php case 'menuListFeeds': ?>
           <?php if ($listFeeds == 'show') { ?>
           <li>
-            <a href="<?php echo $query.'listFeeds=hide'; ?>" title="Hide the feeds list">
-              <span class="menu-ico ico-list-feeds-hide">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-list-feeds-line-5"></span>
-                </span>
-              </span>
+            <a href="<?php echo $query.'listFeeds=hide'; ?>" title="Hide the feeds list" class="menu-ico ico-list-feeds-hide">
               <span class="menu-text menu-list-feeds-hide">
                 Hide feeds list
               </span>
@@ -86,16 +55,7 @@
           </li>
           <?php } else { ?>
           <li>
-            <a href="<?php echo $query.'listFeeds=show'; ?>" title="Show the feeds list">
-              <span class="menu-ico ico-list-feeds-show">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-list-feeds-line-1"></span>
-                  <span class="ico-list-feeds-line-2"></span>
-                  <span class="ico-list-feeds-line-3"></span>
-                  <span class="ico-list-feeds-line-4"></span>
-                </span>
-              </span>
+            <a href="<?php echo $query.'listFeeds=show'; ?>" title="Show the feeds list" class="menu-ico ico-list-feeds-show">
               <span class="menu-text menu-list-feeds-show">
                 Show feeds list
               </span>
@@ -106,18 +66,7 @@
           <?php case 'menuFilter': ?>
           <?php if ($filter === 'unread') { ?>
           <li>
-            <a href="<?php echo $query.'filter=all'; ?>" title="Filter: show all (read and unread) items">
-              <span class="menu-ico ico-filter-all">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-item-circle-1"></span>
-                  <span class="ico-item-circle-2"></span>
-                  <span class="ico-item-circle-3"></span>
-                  <span class="ico-item-line-1"></span>
-                  <span class="ico-item-line-2"></span>
-                  <span class="ico-item-line-3"></span>
-                </span>
-              </span>
+            <a href="<?php echo $query.'filter=all'; ?>" title="Filter: show all (read and unread) items" class="menu-ico ico-filter-all">
               <span class="menu-text menu-filter-all">
                 Show all items
               </span>
@@ -125,16 +74,7 @@
           </li>
           <?php } else { ?>
           <li>
-            <a href="<?php echo $query.'filter=unread'; ?>" title="Filter: show unread items">
-              <span class="menu-ico ico-filter-unread">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-item-circle-1"></span>
-                  <span class="ico-item-circle-2"></span>
-                  <span class="ico-item-line-1"></span>
-                  <span class="ico-item-line-2"></span>
-                </span>
-              </span>
+            <a href="<?php echo $query.'filter=unread'; ?>" title="Filter: show unread items" class="menu-ico ico-filter-unread">
               <span class="menu-text menu-filter-unread">
                 Show unread items
               </span>
@@ -145,14 +85,7 @@
           <?php case 'menuOrder': ?>
           <?php if ($order === 'newerFirst') { ?>
           <li>
-            <a href="<?php echo $query.'order=olderFirst'; ?>" title="Show older first items">
-              <span class="menu-ico ico-order-older">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-w-triangle-down"></span>
-                  <span class="ico-w-line-v"></span>
-                </span>
-              </span>
+            <a href="<?php echo $query.'order=olderFirst'; ?>" title="Show older first items" class="menu-ico ico-order-older">
               <span class="menu-text menu-order">
                 Show older first
               </span>
@@ -160,14 +93,7 @@
           </li>
           <?php } else { ?>
           <li>
-            <a class="repeat" href="<?php echo $query.'order=newerFirst'; ?>" title="Show newer first items">
-              <span class="menu-ico ico-order-newer">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-w-triangle-up"></span>
-                  <span class="ico-w-line-v"></span>
-                </span>
-              </span>
+            <a href="<?php echo $query.'order=newerFirst'; ?>" title="Show newer first items" class="menu-ico ico-order-newer">
               <span class="menu-text menu-order">         
                 Show newer first
               </span>
@@ -177,13 +103,7 @@
           <?php break; ?>
           <?php case 'menuUpdate': ?>
           <li>
-            <a href="<?php echo $query.'update='.$currentHash; ?>" title="Update <?php echo $currentHashType; ?> manually">
-              <span class="menu-ico ico-update">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-update-circle"></span>
-                  <span class="ico-update-triangle"></span>
-                </span>
+            <a href="<?php echo $query.'update='.$currentHash; ?>" title="Update <?php echo $currentHashType; ?> manually" class="menu-ico ico-update">
               </span>
               <span class="menu-text menu-update">
                 Update <?php echo $currentHashType; ?>
@@ -193,16 +113,7 @@
           <?php break; ?>
           <?php case 'menuRead': ?>
           <li>
-            <a href="<?php echo $query.'read='.$currentHash; ?>" title="Mark <?php echo $currentHashType; ?> as read">
-              <span class="menu-ico ico-mark-as-read">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-eye-triangle-left"></span>
-                  <span class="ico-eye-triangle-right"></span>
-                  <span class="ico-eye-circle-1"></span>
-                  <span class="ico-eye-circle-2"></span>
-                </span>
-              </span>
+            <a href="<?php echo $query.'read='.$currentHash; ?>" title="Mark <?php echo $currentHashType; ?> as read" class="menu-ico ico-mark-as-read">
               <span class="menu-text menu-mark-as-read">
                 Mark <?php echo $currentHashType; ?> as read
               </span>
@@ -211,15 +122,7 @@
           <?php break; ?>
           <?php case 'menuUnread': ?>
           <li>
-            <a href="<?php echo $query.'unread='.$currentHash; ?>" title="Mark <?php echo $currentHashType; ?> as unread">
-              <span class="menu-ico ico-mark-as-unread">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-eye-triangle-left"></span>
-                  <span class="ico-eye-triangle-right"></span>
-                  <span class="ico-eye-circle-3"></span>
-                </span>
-              </span>
+            <a href="<?php echo $query.'unread='.$currentHash; ?>" title="Mark <?php echo $currentHashType; ?> as unread" class="menu-ico ico-mark-as-unread">
               <span class="menu-text menu-mark-as-unread">
                 Mark <?php echo $currentHashType; ?> as unread
               </span>
@@ -228,17 +131,7 @@
           <?php break; ?>
           <?php case 'menuEdit': ?>
           <li>
-            <a href="<?php echo $query.'edit='.$currentHash; ?>" title="Edit <?php echo $currentHashType; ?>">
-              <span class="menu-ico ico-edit">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-edit-square"></span>
-                  <span class="ico-edit-circle-1"></span>
-                  <span class="ico-edit-circle-2"></span>
-                  <span class="ico-edit-circle-3"></span>
-                  <span class="ico-edit-circle-4"></span>
-                </span>
-              </span>
+            <a href="<?php echo $query.'edit='.$currentHash; ?>" title="Edit <?php echo $currentHashType; ?>" class="menu-ico ico-edit">
               <span class="menu-text menu-edit">
                 Edit <?php echo $currentHashType; ?>
               </span>
@@ -247,14 +140,7 @@
           <?php break; ?>
           <?php case 'menuAdd': ?>
           <li>
-            <a href="<?php echo $query.'add'; ?>" title="Add a new feed">
-              <span class="menu-ico ico-add-feed">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-w-line-h"></span>
-                  <span class="ico-w-line-v"></span>
-                </span>
-              </span>
+            <a href="<?php echo $query.'add'; ?>" title="Add a new feed" class="menu-ico ico-add-feed">
               <span class="menu-text menu-add-feed">
                 Add a new feed
               </span>
@@ -263,16 +149,7 @@
           <?php break; ?>
           <?php case 'menuHelp': ?>
           <li>
-            <a href="<?php echo $query.'help'; ?>" title="Help : how to use KrISS feed">
-              <span class="menu-ico ico-help">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-help-line-1"></span>
-                  <span class="ico-help-line-2"></span>
-                  <span class="ico-help-line-3"></span>
-                  <span class="ico-help-line-4"></span>
-                </span>
-              </span>
+            <a href="<?php echo $query.'help'; ?>" title="Help : how to use KrISS feed" class="menu-ico ico-help">
               <span class="menu-text menu-help">
                 Help
               </span>
@@ -283,7 +160,11 @@
              if($template === 'index'){
              ?>
           <li>
-            <a href="<?php echo $query.'stars'; ?>" title="Show starred items">Starred Items</a>
+            <a href="<?php echo $query.'stars'; ?>" title="Show starred items" class="menu-ico ico-star">
+              <span class="menu-text menu-help">
+                Starred Items
+              </span>
+            </a>
           </li>
           <?php }
              break; ?>
@@ -293,15 +174,7 @@
           <?php } ?>
           <?php if ($kf->kfc->isLogged()) { ?>
           <li>
-            <a href="?config" title="Configuration">
-              <span class="menu-ico ico-config">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-config-circle-1"></span>
-                  <span class="ico-config-circle-2"></span>
-                  <span class="ico-config-circle-3"></span>
-                </span>
-              </span>
+            <a href="?config" title="Configuration" class="menu-ico ico-config">
               <span class="menu-text menu-config">
                 Configuration
               </span>
@@ -310,28 +183,14 @@
           <?php } ?>
           <?php if (Session::isLogged()) { ?>
           <li>
-            <a href="?logout" title="Logout">
-              <span class="menu-ico ico-logout">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-onoff-circle"></span>
-                  <span class="ico-onoff-line"></span>
-                </span>
-              </span>
+            <a href="?logout" title="Logout" class="menu-ico ico-logout">
               <span class="menu-text menu-logout">
                 Logout
               </span>
             </a></li>
           <?php } else { ?>
           <li>
-            <a href="?login">
-              <span class="menu-ico ico-login">
-                <span class="ico">
-                  <span class="ico-b-disc"></span>
-                  <span class="ico-onoff-circle"></span>
-                  <span class="ico-onoff-line"></span>
-                </span>
-              </span>
+            <a href="?login" class="menu-ico ico-login">
               <span class="menu-text menu-login">
                 Login
               </span>
