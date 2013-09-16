f308ca8bab78a368258b0b848288a3ebe1f4fbda
Tontof <tontof@free.fr>
2013-09-16 22:27:56 +0200

    add zip to readData

--- src/bin/readData.php
+++ src/bin/readData.php
@@ -1,24 +1,25 @@
 <?php
 
 if ($argc < 3) {
-    die('php readData.php data.php [data|raw]'."\n");
+    die('php readData.php data.php [zip|raw|data]'."\n");
 }
 
 define('PHPPREFIX', '<?php /* '); // Prefix to encapsulate data in php code.
 define('PHPSUFFIX', ' */ ?>'); // Suffix to encapsulate data in php code.
 
-$data = gzinflate(
-    base64_decode(
-        substr(
-            file_get_contents($argv[1]),
-            strlen(PHPPREFIX),
-            -strlen(PHPSUFFIX)
-        )
+$data = base64_decode(
+    substr(
+        file_get_contents($argv[1]),
+        strlen(PHPPREFIX),
+        -strlen(PHPSUFFIX)
     )
 );
 
-if ($argv[2] === 'data') {
-    $data = unserialize($data);
+if ($argv[2] !== 'zip') {
+    $data = gzinflate($data);
+    if ($argv[2] === 'data') {
+        $data = unserialize($data);
+    }
 }
 
 var_dump($data);
